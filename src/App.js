fb9a5e896dce8db4b160b1d62fe4a1c6d3ffa78c
Anderson <anderson771218@gmail.com>
2019-08-22 02:37:50 +0800

    Implement checkout-item component, added addItem and removeItem functionality to checkout item component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./App.css";
 //import Layout from './components/Layout/Layout'
 import { Route, Switch, Redirect } from "react-router-dom";
+
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
@@ -44,7 +47,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route path="/" exact component={Homepage} />
+          <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
           <Route
             exact
@@ -57,6 +60,7 @@ class App extends React.Component {
               )
             }
           />
+          <Route exact path="/checkout" component={CheckoutPage} />
         </Switch>
       </div>
     );
