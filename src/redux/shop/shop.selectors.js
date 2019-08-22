179b34e4c6c30fef65e264f11715d7ac02e5752b
Anderson <anderson771218@gmail.com>
2019-08-23 01:49:28 +0800

    Add new components: collections-overview, collection( which is a page). Move state of directory and shop to redux store

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from "reselect";
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+//transform object to array for usage in collections-overview component
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+//currying
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
