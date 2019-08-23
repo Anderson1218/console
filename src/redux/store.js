6e984d627531c362ce2011bc2c440778db8f5aa9
Anderson <anderson771218@gmail.com>
2019-08-24 00:47:49 +0800

    remove Redux devtools from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,18 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 const middlewares = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+}
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(...middlewares))
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 const persistor = persistStore(store);
 
-// export default { store, persistor };
 export { store, persistor };
