ed812d0782632c3e68f1869bc9968c1c4272d7a9
Anderson <anderson771218@gmail.com>
2019-08-23 23:50:50 +0800

    Stripe integration, change favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  //stripe requires price in cents
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_XVXMKAffgMXfx8XPTHK4EfeD00hfUqDTDH";
+  const onToken = token => {
+    //make API call to Backend
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Me!"
+      name="An Store"
+      billingAddress
+      shippingAddress
+      image=""
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Give Money"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
