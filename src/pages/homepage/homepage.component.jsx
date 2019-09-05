795b7ae0ce833ceebb7f42ffd12957c8ee0297c5
Anderson <anderson771218@gmail.com>
2019-09-05 14:22:02 +0800

    Delete useless files

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import Directory from "../../components/directory/directory.component";
 import Chat from "../../components/chat/chat.component";
 
 const HomePage = () => <Chat />;
